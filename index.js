6df400e6fd4ed58a57257b8982ab3e10c808a2eb
ekmartin <ekmartinb@gmail.com>
2014-09-28 13:16:49 +0200

    Small fixes

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var express     = require('express')
   , app         = module.exports = express()
   , server      = require('http').Server(app)
   , io          = require('socket.io')(server)
-  , stylus      = require('stylus')
-  , nib         = require('nib')
   , colors      = require('colors')
   , WordList    = require('./app/server/word-list')
   , gameServer  = require('./app/server/server');
@@ -15,11 +13,6 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/app/views');
 
-app.use(stylus.middleware({
-  src: __dirname + '/assets',
-  dest: __dirname + '/public',
-  compile: function(str, path) {return stylus(str).set('filename', path).use(nib());}
-}));
 app.use(express.static(__dirname + '/public'));
 
 require('./app/config/routes')(app);
